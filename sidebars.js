b08f47830b596881a52fcc1bb1b455cdee88d02a
geleeroyale <admin@geleeroyale.work>
2021-01-24 21:56:09 +0100

    fix build issue after changing the id of a page

--- sidebars.js
+++ sidebars.js
@@ -1,21 +1,11 @@
 module.exports = {
   someSidebar: {
-    'Getting Started':
-      [
-        'getting-started-welcome',
-        'getting-started-contributing',
-        'events-welcome',
-      ],
-    'Academy':
-      [
-        'academy-welcome',
-        'academy-tmg1-ecosystem',
-      ],
-    'Library':
-      [
-        'library-welcome',
-        'library-te-101',
-        'library-te-book',
-      ],
-  },
-};
+    'Getting Started': [
+      'getting-started-welcome',
+      'getting-started-ourcommunity',
+      'events-welcome'
+    ],
+    Academy: ['academy-welcome', 'academy-tmg1-ecosystem'],
+    Library: ['library-welcome', 'library-te-101', 'library-te-book']
+  }
+}
